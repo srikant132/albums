46dbaecd1305b4d9d95c0b4d4dd9e4da52fd2fa5
sriaknt132 <srikantcs132@gmail.com>
2019-02-10 16:51:29 +0530

    app initial commitment.

--- index.js
+++ index.js
@@ -2,9 +2,22 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
+//import a library to help create  a registerComponent
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Header } from './src/components/Header';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+//create a registerComponent
+const App = () => (
+     <Header />
+  );
 
-AppRegistry.registerComponent(appName, () => App);
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
+
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+//
+// AppRegistry.registerComponent(appName, () => App);
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+//import a library to help create  a registerComponent
+import React from 'react';
+import { Text } from 'react-native';
+
+//Make a Componant
+const Header = () => {
+   return <Text>Albums!</Text>;
+ };
+
+ //Make the component available to  other parts of the app
+ export default Header;
